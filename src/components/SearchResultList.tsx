00c0277176cf4e822592cb22e3bc2d5ea7394f89
ShinJaeIl01 <ekvm555@naver.com>
2023-09-06 19:18:50 +0900

    feat: 목록 스크롤 이동 구현

--- src/components/SearchResultList.tsx
+++ src/components/SearchResultList.tsx
@@ -1,24 +1,74 @@
 import { SearchApiResponse } from "../types";
 import "../styles/searchResultListStyle.scss";
+import { useEffect, useRef, useState } from "react";
 
 interface Props {
     resultData: SearchApiResponse | null;
     isFetching: boolean;
 }
 
+const KEYBOARD_MOVE_NUMBER = 1;
+const IDX_PLUS = 1;
+
 const SearchResultList = ({ resultData, isFetching }: Props) => {
+    const listUlRef = useRef<HTMLUListElement>(null);
+
+    const [selectItem, setSelectItem] = useState<number>(0);
+
+    const handleKeyDown = (event: KeyboardEvent) => {
+        if (resultData) {
+            if (event.key === "ArrowUp") {
+                setSelectItem((prev) => {
+                    return prev === 1 ? 1 : prev - KEYBOARD_MOVE_NUMBER;
+                });
+            } else if (event.key === "ArrowDown") {
+                setSelectItem((prev) => {
+                    return prev === resultData.length
+                        ? prev
+                        : prev + KEYBOARD_MOVE_NUMBER;
+                });
+            }
+        }
+    };
+
+    const moveScroll = () => {
+        if (listUlRef.current) {
+            listUlRef.current.scrollTop = 40 * selectItem - 40;
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener("keydown", handleKeyDown);
+
+        moveScroll();
+
+        return () => {
+            window.removeEventListener("keydown", handleKeyDown);
+        };
+    }, [resultData, selectItem]);
+
     if (isFetching) return <span>로딩 중....</span>;
 
     if (resultData)
         return (
-            <ul>
+            <ul ref={listUlRef} className="ulBox">
                 {resultData.length > 0 ? (
                     <>
-                        {resultData.map((result) => (
-                            <li key={result.sickCd} className="listItem">
-                                - {result.sickNm}
-                            </li>
-                        ))}
+                        {resultData.map((result, idx) => {
+                            const listIdx = idx + IDX_PLUS;
+                            return (
+                                <li
+                                    key={result.sickCd}
+                                    className={`listItem ${
+                                        selectItem === listIdx
+                                            ? "selectItem"
+                                            : ""
+                                    }`}
+                                >
+                                    - {result.sickNm}
+                                </li>
+                            );
+                        })}
                     </>
                 ) : (
                     <li>
